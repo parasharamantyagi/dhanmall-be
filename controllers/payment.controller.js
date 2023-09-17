de54760f36662e5bc4e46087b3b4f386ffa7c337
aman_tevvo <aman.tyagi@tevvo.com>
2023-09-17 16:49:35 +0530

    price set

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -7,7 +7,11 @@ const {
 } = require("../helpers");
 const { setDataType } = require("../helpers");
 const { objectFormat } = require("../helpers");
-const { saveMyChildren, getMyChildren, updateMyChildren } = require("../models/my_childrens");
+const {
+  saveMyChildren,
+  getMyChildren,
+  updateMyChildren,
+} = require("../models/my_childrens");
 const { savePayment } = require("../models/payments");
 const {
   plusUserMoney,
@@ -27,36 +31,60 @@ exports.addPayment = async (req, res, next) => {
     ]);
     plusUserMoney(req.user.user_id, { money: inputData.ammount }, "payment");
     savePayment(inputData);
-    updateUserFromId(req.user.user_id,{first_payment: 1});
+    updateUserFromId(req.user.user_id, { first_payment: 1 });
     let dicountAdmin = await getMyChildren({ childrens_id: req.user.user_id });
 
     let resLavel1 = arrayOfObject(dicountAdmin, { type: "lavel_1" });
     let resLavel2 = arrayOfObject(dicountAdmin, { type: "lavel_2" });
     let resLavel3 = arrayOfObject(dicountAdmin, { type: "lavel_3" });
     let resLavel4 = arrayOfObject(dicountAdmin, { type: "lavel_4" });
-    if(checkObj(resLavel1)){
-      let ammount = (setDataType(inputData.ammount) * USER_COMMISION.LAVEL_1) / 100;
-      updateMyChildren(resLavel1._id,{first_reward: ammount});
-      plusUserMoney(resLavel1.user_id, { money: ammount },'reward');
-      savePayment(merge_object(inputData,{user_id: resLavel1.user_id, type: 'reward_1'}));
+    if (checkObj(resLavel1)) {
+      let ammount =
+        (setDataType(inputData.ammount) * USER_COMMISION.LAVEL_1) / 100;
+      updateMyChildren(resLavel1._id, { first_reward: ammount });
+      plusUserMoney(resLavel1.user_id, { money: ammount }, "reward");
+      savePayment(
+        merge_object(inputData, {
+          user_id: resLavel1.user_id,
+          type: "reward_1",
+        })
+      );
     }
-    if(checkObj(resLavel2)){
-      let ammount = (setDataType(inputData.ammount) * USER_COMMISION.LAVEL_2) / 100;
-      updateMyChildren(resLavel2._id,{first_reward: ammount});
-      plusUserMoney(resLavel2.user_id, { money: ammount },'reward');
-      savePayment(merge_object(inputData,{user_id: resLavel2.user_id, type: 'reward_1'}));
+    if (checkObj(resLavel2)) {
+      let ammount =
+        (setDataType(inputData.ammount) * USER_COMMISION.LAVEL_2) / 100;
+      updateMyChildren(resLavel2._id, { first_reward: ammount });
+      plusUserMoney(resLavel2.user_id, { money: ammount }, "reward");
+      savePayment(
+        merge_object(inputData, {
+          user_id: resLavel2.user_id,
+          type: "reward_1",
+        })
+      );
     }
-    if(checkObj(resLavel3)){
-      let ammount = (setDataType(inputData.ammount) * USER_COMMISION.LAVEL_3) / 100;
-      updateMyChildren(resLavel3._id,{first_reward: ammount});
-      plusUserMoney(resLavel3.user_id, { money: ammount },'reward');
-      savePayment(merge_object(inputData,{user_id: resLavel3.user_id, type: 'reward_1'}));
+    if (checkObj(resLavel3)) {
+      let ammount =
+        (setDataType(inputData.ammount) * USER_COMMISION.LAVEL_3) / 100;
+      updateMyChildren(resLavel3._id, { first_reward: ammount });
+      plusUserMoney(resLavel3.user_id, { money: ammount }, "reward");
+      savePayment(
+        merge_object(inputData, {
+          user_id: resLavel3.user_id,
+          type: "reward_1",
+        })
+      );
     }
-    if(checkObj(resLavel4)){
-      let ammount = (setDataType(inputData.ammount) * USER_COMMISION.LAVEL_4) / 100;
-      updateMyChildren(resLavel4._id,{first_reward: ammount});
-      plusUserMoney(resLavel4.user_id, { money: ammount },'reward');
-      savePayment(merge_object(inputData,{user_id: resLavel4.user_id, type: 'reward_1'}));
+    if (checkObj(resLavel4)) {
+      let ammount =
+        (setDataType(inputData.ammount) * USER_COMMISION.LAVEL_4) / 100;
+      updateMyChildren(resLavel4._id, { first_reward: ammount });
+      plusUserMoney(resLavel4.user_id, { money: ammount }, "reward");
+      savePayment(
+        merge_object(inputData, {
+          user_id: resLavel4.user_id,
+          type: "reward_1",
+        })
+      );
     }
     return res.status(200).json({
       status: 1,
