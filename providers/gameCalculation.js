7c236dd8f49b9c0299aee4bb88a485568e948c23
aman_tevvo <aman.tyagi@tevvo.com>
2023-09-20 11:27:05 +0530

    commit code

--- providers/gameCalculation.js
+++ providers/gameCalculation.js
@@ -1,5 +1,11 @@
 const { GDM_MODULE } = require("../config");
-const { filterArrayKey, arrayOfObject, sum_of_array, setDataType } = require("../helpers");
+const {
+  filterArrayKey,
+  arrayOfObject,
+  sum_of_array,
+  setDataType,
+} = require("../helpers");
+const { colors2 } = require("./colors");
 
 module.exports.calCulationNumberPridiction = (all_orders) => {
   let pick_red = arrayOfObject(all_orders, { pick: "red" }, Array);
@@ -16,106 +22,98 @@ module.exports.calCulationNumberPridiction = (all_orders) => {
   let pick_8 = arrayOfObject(all_orders, { pick: "8" }, Array);
   let pick_9 = arrayOfObject(all_orders, { pick: "9" }, Array);
 
-  let price_pick_red = filterArrayKey(pick_red, "delivery");
-  let price_pick_green = filterArrayKey(pick_green, "delivery");
-  let price_pick_violet = filterArrayKey(pick_violet, "delivery");
-  let price_pick_0 = filterArrayKey(pick_0, "delivery");
-  let price_pick_1 = filterArrayKey(pick_1, "delivery");
-  let price_pick_2 = filterArrayKey(pick_2, "delivery");
-  let price_pick_3 = filterArrayKey(pick_3, "delivery");
-  let price_pick_4 = filterArrayKey(pick_4, "delivery");
-  let price_pick_5 = filterArrayKey(pick_5, "delivery");
-  let price_pick_6 = filterArrayKey(pick_6, "delivery");
-  let price_pick_7 = filterArrayKey(pick_7, "delivery");
-  let price_pick_8 = filterArrayKey(pick_8, "delivery");
-  let price_pick_9 = filterArrayKey(pick_9, "delivery");
+  let price_pick_red = sum_of_array(filterArrayKey(pick_red, "delivery"));
+  let price_pick_green = sum_of_array(filterArrayKey(pick_green, "delivery"));
+  let price_pick_violet = sum_of_array(filterArrayKey(pick_violet, "delivery"));
+  let price_pick_0 = sum_of_array(filterArrayKey(pick_0, "delivery"));
+  let price_pick_1 = sum_of_array(filterArrayKey(pick_1, "delivery"));
+  let price_pick_2 = sum_of_array(filterArrayKey(pick_2, "delivery"));
+  let price_pick_3 = sum_of_array(filterArrayKey(pick_3, "delivery"));
+  let price_pick_4 = sum_of_array(filterArrayKey(pick_4, "delivery"));
+  let price_pick_5 = sum_of_array(filterArrayKey(pick_5, "delivery"));
+  let price_pick_6 = sum_of_array(filterArrayKey(pick_6, "delivery"));
+  let price_pick_7 = sum_of_array(filterArrayKey(pick_7, "delivery"));
+  let price_pick_8 = sum_of_array(filterArrayKey(pick_8, "delivery"));
+  let price_pick_9 = sum_of_array(filterArrayKey(pick_9, "delivery"));
 
-  let total_Red_delivery =
-    sum_of_array(price_pick_red) +
-    sum_of_array(price_pick_2) +
-    sum_of_array(price_pick_4) +
-    sum_of_array(price_pick_6) +
-    sum_of_array(price_pick_8);
-  let total_Green_delivery =
-    sum_of_array(price_pick_green) +
-    sum_of_array(price_pick_1) +
-    sum_of_array(price_pick_3) +
-    sum_of_array(price_pick_7) +
-    sum_of_array(price_pick_9);
-  let total_Violet_delivery = sum_of_array(price_pick_violet);
-  let total_Red_Violet_delivery = sum_of_array(price_pick_0);
-  let total_Green_Violet_delivery = sum_of_array(price_pick_5);
+  let total_Red_delivery = sum_of_array([
+    price_pick_red,
+    price_pick_2,
+    price_pick_4,
+    price_pick_6,
+    price_pick_8,
+  ]);
+  let total_Green_delivery = sum_of_array([
+    price_pick_green,
+    price_pick_1,
+    price_pick_3,
+    price_pick_7,
+    price_pick_9,
+  ]);
+  let total_Violet_delivery = sum_of_array([price_pick_violet]);
+  let total_Red_Violet_delivery = sum_of_array([price_pick_0]);
+  let total_Green_Violet_delivery = sum_of_array([price_pick_5]);
 
-  let unit = 0;
-  let color = "red";
-  if (total_Red_delivery >= total_Green_delivery) {
-    color = "green";
-  }
+  let unit = 2;
 
   if (total_Red_delivery < total_Green_delivery) {
     if (
-      sum_of_array(price_pick_2) < sum_of_array(price_pick_4) &&
-      sum_of_array(price_pick_2) < sum_of_array(price_pick_6) &&
-      sum_of_array(price_pick_2) < sum_of_array(price_pick_8)
+      price_pick_2 <= price_pick_4 &&
+      price_pick_2 <= price_pick_6 &&
+      price_pick_2 <= price_pick_8
     ) {
       unit = 2;
-    }
-    if (
-      sum_of_array(price_pick_4) < sum_of_array(price_pick_2) &&
-      sum_of_array(price_pick_4) < sum_of_array(price_pick_6) &&
-      sum_of_array(price_pick_4) < sum_of_array(price_pick_8)
+    } else if (
+      price_pick_4 <= price_pick_2 &&
+      price_pick_4 <= price_pick_6 &&
+      price_pick_4 <= price_pick_8
     ) {
       unit = 4;
-    }
-    if (
-      sum_of_array(price_pick_6) < sum_of_array(price_pick_2) &&
-      sum_of_array(price_pick_6) < sum_of_array(price_pick_4) &&
-      sum_of_array(price_pick_6) < sum_of_array(price_pick_8)
+    } else if (
+      price_pick_6 <= price_pick_2 &&
+      price_pick_6 <= price_pick_4 &&
+      price_pick_6 <= price_pick_8
     ) {
       unit = 6;
-    }
-    if (
-      sum_of_array(price_pick_8) < sum_of_array(price_pick_2) &&
-      sum_of_array(price_pick_8) < sum_of_array(price_pick_4) &&
-      sum_of_array(price_pick_8) < sum_of_array(price_pick_6)
+    } else if (
+      price_pick_8 <= price_pick_2 &&
+      price_pick_8 <= price_pick_4 &&
+      price_pick_8 <= price_pick_6
     ) {
       unit = 8;
     }
-  }
-
-  if (total_Red_delivery > total_Green_delivery) {
+  } else if (total_Red_delivery > total_Green_delivery) {
     if (
-      sum_of_array(price_pick_1) < sum_of_array(price_pick_3) &&
-      sum_of_array(price_pick_1) < sum_of_array(price_pick_7) &&
-      sum_of_array(price_pick_1) < sum_of_array(price_pick_9)
+      price_pick_1 <= price_pick_3 &&
+      price_pick_1 <= price_pick_7 &&
+      price_pick_1 <= price_pick_9
     ) {
       unit = 1;
-    }
-    if (
-      sum_of_array(price_pick_3) < sum_of_array(price_pick_1) &&
-      sum_of_array(price_pick_3) < sum_of_array(price_pick_7) &&
-      sum_of_array(price_pick_3) < sum_of_array(price_pick_9)
+    } else if (
+      price_pick_3 <= price_pick_1 &&
+      price_pick_3 <= price_pick_7 &&
+      price_pick_3 <= price_pick_9
     ) {
       unit = 3;
-    }
-    if (
-      sum_of_array(price_pick_7) < sum_of_array(price_pick_1) &&
-      sum_of_array(price_pick_7) < sum_of_array(price_pick_3) &&
-      sum_of_array(price_pick_7) < sum_of_array(price_pick_9)
+    } else if (
+      price_pick_7 <= price_pick_1 &&
+      price_pick_7 <= price_pick_3 &&
+      price_pick_7 <= price_pick_9
     ) {
       unit = 7;
-    }
-    if (
-      sum_of_array(price_pick_9) < sum_of_array(price_pick_1) &&
-      sum_of_array(price_pick_9) < sum_of_array(price_pick_3) &&
-      sum_of_array(price_pick_9) < sum_of_array(price_pick_7)
+    } else if (
+      price_pick_9 <= price_pick_1 &&
+      price_pick_9 <= price_pick_3 &&
+      price_pick_9 <= price_pick_7
     ) {
       unit = 9;
     }
   }
   let otp = GDM_MODULE.rn({ min: 1222, max: 1599, integer: true });
-  let calPrice = setDataType(otp,'s') + setDataType(unit,'s');
-  calPrice = setDataType(calPrice,'n');
+  let calPrice = setDataType(otp, "s") + setDataType(unit, "s");
+  calPrice = setDataType(calPrice, "n");
+
+  let color = arrayOfObject(colors2, { id: unit }, "colour");
 
   // let red_color_price = filterArrayKey(pick_red, 'delivery');
   // let green_color_price = filterArrayKey(pick_green, 'delivery');
@@ -124,5 +122,32 @@ module.exports.calCulationNumberPridiction = (all_orders) => {
   // green_color_price = checkArray(green_color_price) ? sum_of_array(green_color_price) : 0;
   // let color_data = red_color_price > green_color_price ? 'green' : 'red';
   // return {total_Red_delivery, total_Green_delivery,  total_Violet_delivery, total_Red_Violet_delivery, total_Green_Violet_delivery};
-  return { status: 1, unit: unit, price: calPrice, color: color };
+
+  return {
+    status: 1,
+    unit: unit,
+    price: calPrice,
+    color: color,
+    // data: {
+    //   color: {
+    //     total_Red_delivery,
+    //     total_Green_delivery,
+    //     total_Violet_delivery,
+    //     total_Red_Violet_delivery,
+    //     total_Green_Violet_delivery,
+    //   },
+    //   number: {
+    //     pick_0: price_pick_0,
+    //     pick_1: price_pick_1,
+    //     pick_2: price_pick_2,
+    //     pick_3: price_pick_3,
+    //     pick_4: price_pick_4,
+    //     pick_5: price_pick_5,
+    //     pick_6: price_pick_6,
+    //     pick_7: price_pick_7,
+    //     pick_8: price_pick_8,
+    //     pick_8: price_pick_9,
+    //   },
+    // },
+  };
 };
