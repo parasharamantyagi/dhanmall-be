ab00fd18c247a925ef2d53554e32331c1cdedda6
aman_tevvo <aman.tyagi@tevvo.com>
2023-09-23 22:31:10 +0530

    cron job cal

--- controllers/cron.job.controller.js
+++ controllers/cron.job.controller.js
@@ -8,6 +8,7 @@ const {
   merge_object,
   setDataType,
   todayDate,
+  check,
 } = require("../helpers");
 const { saveGame, gameById, updateGame } = require("../models/Games");
 const { getOrderCalculation } = require("../models/OrderCalculation");
@@ -18,12 +19,40 @@ exports.gameInterval = async (req, res, next) => {
   try {
     let gameId = await gameById();
     let all_orders = [];
+    let order_cal = { amount: 0 };
     if (checkObj(gameId)) {
-      all_orders = await getOrderCalculation(setDataType(gameId._id,'s'));
+      all_orders = await getOrderCalculation(setDataType(gameId._id, "s"));
       let calResult = calCulationNumberPridiction(all_orders);
       updateGame(setDataType(gameId._id,'s'), calResult);
-      for(let order of all_orders){
-        await updateOrder(setDataType(order._id,'s'),merge_object(calResult,{amount: 10}));
+      for (let order of all_orders) {
+        if (order.type === 2) {
+          if (order.pick === calResult.color) {
+            order_cal.amount = order.delivery * 9;
+            order_cal.status = 1;
+          } else {
+            order_cal.amount = 0;
+            order_cal.status = 2;
+          }
+        } else {
+          if (order.pick === calResult.color) {
+            order_cal.amount = order.delivery * 2;
+            order_cal.status = 1;
+          } else {
+            order_cal.amount = 0;
+            order_cal.status = 2;
+          }
+        }
+        await updateOrder(
+          setDataType(order.order_id, "s"),
+          merge_object(
+            {
+              unit: calResult.unit,
+              color: calResult.color,
+              price: calResult.price,
+            },
+            order_cal
+          )
+        );
       }
     }
     let period = setDataType(1,'padStart');
@@ -35,7 +64,6 @@ exports.gameInterval = async (req, res, next) => {
       }
     }
     saveGame({ period: period, project_id: 1, price: 0 , begintime: currentDate() , date: todayDate()});
-
     return res.status(200).json(true);
   } catch (e) {
     return res.json({ status: 0, message: e.message });
