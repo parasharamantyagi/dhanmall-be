227672ab1d04f5221b359f29732261603f89672b
aman_tevvo <aman.tyagi@tevvo.com>
2023-10-07 15:03:30 +0530

    fix code

--- providers/gameCalculation.js
+++ providers/gameCalculation.js
@@ -3,69 +3,86 @@ const {
   arrayOfObject,
   setDataType,
   array_to_str,
+  checkObj,
 } = require("../helpers");
 const { colors2 } = require("./colors");
 
 module.exports.calCulationNumberPridiction = (gameOrders) => {
   let unit = GDM_MODULE.rn({ min: 0, max: 9, integer: true });
-  const { pick_red, pick_green, pick_violet, pick_0, pick_1, pick_2, pick_3, pick_4, pick_5, pick_6, pick_7, pick_8, pick_9, total_price } = gameOrders;
-  if(total_price.total_delivery){
-    if (pick_red.total_delivery < pick_green.total_delivery) {
-      if (
-        pick_2.total_delivery <= pick_4.total_delivery &&
-        pick_2.total_delivery <= pick_6.total_delivery &&
-        pick_2.total_delivery <= pick_8.total_delivery
-      ) {
-        unit = 2;
-      } else if (
-        pick_4.total_delivery <= pick_2.total_delivery &&
-        pick_4.total_delivery <= pick_6.total_delivery &&
-        pick_4.total_delivery <= pick_8.total_delivery
-      ) {
-        unit = 4;
-      } else if (
-        pick_6.total_delivery <= pick_2.total_delivery &&
-        pick_6.total_delivery <= pick_4.total_delivery &&
-        pick_6.total_delivery <= pick_8.total_delivery
-      ) {
-        unit = 6;
-      } else if (
-        pick_8.total_delivery <= pick_2.total_delivery &&
-        pick_8.total_delivery <= pick_4.total_delivery &&
-        pick_8.total_delivery <= pick_6.total_delivery
-      ) {
-        unit = 8;
+  if (checkObj(gameOrders)) {
+    const {
+      pick_red,
+      pick_green,
+      pick_violet,
+      pick_0,
+      pick_1,
+      pick_2,
+      pick_3,
+      pick_4,
+      pick_5,
+      pick_6,
+      pick_7,
+      pick_8,
+      pick_9,
+      total_price,
+    } = gameOrders;
+    if (total_price.total_delivery) {
+      if (pick_red.total_delivery < pick_green.total_delivery) {
+        if (
+          pick_2.total_delivery <= pick_4.total_delivery &&
+          pick_2.total_delivery <= pick_6.total_delivery &&
+          pick_2.total_delivery <= pick_8.total_delivery
+        ) {
+          unit = 2;
+        } else if (
+          pick_4.total_delivery <= pick_2.total_delivery &&
+          pick_4.total_delivery <= pick_6.total_delivery &&
+          pick_4.total_delivery <= pick_8.total_delivery
+        ) {
+          unit = 4;
+        } else if (
+          pick_6.total_delivery <= pick_2.total_delivery &&
+          pick_6.total_delivery <= pick_4.total_delivery &&
+          pick_6.total_delivery <= pick_8.total_delivery
+        ) {
+          unit = 6;
+        } else if (
+          pick_8.total_delivery <= pick_2.total_delivery &&
+          pick_8.total_delivery <= pick_4.total_delivery &&
+          pick_8.total_delivery <= pick_6.total_delivery
+        ) {
+          unit = 8;
+        }
+      } else if (pick_red.total_delivery > pick_green.total_delivery) {
+        if (
+          pick_1.total_delivery <= pick_3.total_delivery &&
+          pick_1.total_delivery <= pick_7.total_delivery &&
+          pick_1.total_delivery <= pick_9.total_delivery
+        ) {
+          unit = 1;
+        } else if (
+          pick_3.total_delivery <= pick_1.total_delivery &&
+          pick_3.total_delivery <= pick_7.total_delivery &&
+          pick_3.total_delivery <= pick_9.total_delivery
+        ) {
+          unit = 3;
+        } else if (
+          pick_7.total_delivery <= pick_1.total_delivery &&
+          pick_7.total_delivery <= pick_3.total_delivery &&
+          pick_7.total_delivery <= pick_9.total_delivery
+        ) {
+          unit = 7;
+        } else if (
+          pick_9.total_delivery <= pick_1.total_delivery &&
+          pick_9.total_delivery <= pick_3.total_delivery &&
+          pick_9.total_delivery <= pick_7.total_delivery
+        ) {
+          unit = 9;
+        }
+      } else {
       }
-    } else if (pick_red.total_delivery > pick_green.total_delivery) {
-      if (
-        pick_1.total_delivery <= pick_3.total_delivery &&
-        pick_1.total_delivery <= pick_7.total_delivery &&
-        pick_1.total_delivery <= pick_9.total_delivery
-      ) {
-        unit = 1;
-      } else if (
-        pick_3.total_delivery <= pick_1.total_delivery &&
-        pick_3.total_delivery <= pick_7.total_delivery &&
-        pick_3.total_delivery <= pick_9.total_delivery
-      ) {
-        unit = 3;
-      } else if (
-        pick_7.total_delivery <= pick_1.total_delivery &&
-        pick_7.total_delivery <= pick_3.total_delivery &&
-        pick_7.total_delivery <= pick_9.total_delivery
-      ) {
-        unit = 7;
-      } else if (
-        pick_9.total_delivery <= pick_1.total_delivery &&
-        pick_9.total_delivery <= pick_3.total_delivery &&
-        pick_9.total_delivery <= pick_7.total_delivery
-      ) {
-        unit = 9;
-      }
-    } else {
     }
   }
-
   let otp = GDM_MODULE.rn({ min: 1222, max: 1599, integer: true });
   let calPrice = setDataType(otp, "s") + setDataType(unit, "s");
   calPrice = setDataType(calPrice, "n");
@@ -74,6 +91,6 @@ module.exports.calCulationNumberPridiction = (gameOrders) => {
     status: 1,
     unit: unit,
     price: calPrice,
-    color: array_to_str(color)
+    color: array_to_str(color),
   };
 };
